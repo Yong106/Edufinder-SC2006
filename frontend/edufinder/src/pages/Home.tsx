740f3ebc01b902ec49b0ffc9cbb977e5e09f9e0c
yongcs106 <yong106106@gmail.com>
2025-09-24 14:08:37 +0800

    Initialised frontend React

---
+++ frontend/edufinder/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div className="w-screen h-screen flex flex-col justify-center items-center text-red-900">
+            Hello World
+        </div>
+    )
+}
