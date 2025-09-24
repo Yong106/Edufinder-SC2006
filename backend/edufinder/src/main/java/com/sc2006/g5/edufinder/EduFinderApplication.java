67b45b734303191203b4dd0dd06075a1390519b3
yongcs106 <yong106106@gmail.com>
2025-09-24 13:34:36 +0800

    Initialize backend Spring Boot

---
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/EduFinderApplication.java
@@ -0,0 +1,13 @@
+package com.sc2006.g5.edufinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduFinderApplication.class, args);
+	}
+
+}
---
+++ backend/edufinder/src/test/java/com/sc2006/g5/edufinder/EduFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc2006.g5.edufinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
