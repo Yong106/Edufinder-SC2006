1312eb237fe1bbafc4e61594d8a5d248442da0d9
Yu Wenhao <yuwenhao03@gmail.com>
2025-10-14 10:43:43 +0800

    Change frontend template

--- frontend/edufinder/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home(){
-    return (
-        <div className="w-screen h-screen flex flex-col justify-center items-center text-red-900">
-            Hello World
-        </div>
-    )
-}
