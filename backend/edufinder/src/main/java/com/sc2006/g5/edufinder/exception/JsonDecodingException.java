782b7ed1eee043add9f40ce045000d24cd2a6ae9
yongcs106 <yong106106@gmail.com>
2025-10-23 14:05:14 +0800

    feat(school): add repository to fetch and cache school from API

---
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/exception/JsonDecodingException.java
@@ -0,0 +1,7 @@
+package com.sc2006.g5.edufinder.exception;
+
+public class JsonDecodingException extends RuntimeException{
+    public JsonDecodingException(String message){
+        super(message);
+    }
+}
