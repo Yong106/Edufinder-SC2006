4a5792ad02ad994ff9e472d09a3c034bec980121
yongcs106 <yong106106@gmail.com>
2025-10-23 14:03:26 +0800

    feat(api): add parser to parse gov response to DTO

---
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/dto/api/ApiRecord.java
@@ -0,0 +1,3 @@
+package com.sc2006.g5.edufinder.dto.api;
+
+public abstract class ApiRecord {}
---
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/util/ApiResponseParser.java
@@ -0,0 +1,32 @@
+package com.sc2006.g5.edufinder.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sc2006.g5.edufinder.dto.api.ApiRecord;
+import com.sc2006.g5.edufinder.dto.api.ApiResponse;
+import com.sc2006.g5.edufinder.exception.JsonDecodingException;
+
+@Component
+public class ApiResponseParser {
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ApiResponseParser(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public <R extends ApiRecord> ApiResponse<R> parseApiRecord(String json, Class<R> apiRecordClass) {
+        try {
+            return objectMapper.readValue(
+                json, 
+                objectMapper.getTypeFactory().constructParametricType(ApiResponse.class, apiRecordClass)
+            );
+        } catch (JsonProcessingException e) {
+            throw new JsonDecodingException(e.getMessage());
+        }
+    }
+}
