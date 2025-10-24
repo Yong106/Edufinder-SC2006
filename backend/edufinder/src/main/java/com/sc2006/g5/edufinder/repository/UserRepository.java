ede3b0a92602ae33202922c6a6edb8dd298a3dd4
yongcs106 <yong106106@gmail.com>
2025-10-24 19:53:11 +0800

    feat(user): add JpaRepository to store user

---
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sc2006.g5.edufinder.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sc2006.g5.edufinder.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findOneByUsername(String username);
+}
