7733acfd85ba88c34626257fd16e770db236d00a
Yu Wenhao <yuwenhao03@gmail.com>
2025-11-03 01:02:40 +0800

    Add school page to router

--- edufinder_frontend/src/views/dashboards/SchoolDetails.tsx
+++ edufinder_frontend/src/views/dashboards/SchoolDetails.tsx
@@ -1,17 +1,19 @@
-import { School } from 'src/types/school/school.ts';
 import SchoolInfoCards from 'src/components/dashboard/SchoolInfoCards.tsx';
+import { Navigate, useParams } from 'react-router';
+import { useSchoolContext } from 'src/context/SchoolProvider.tsx';
 
-const SchoolDetails = ({school}: {school: School | null}) => {
+const SchoolDetails = () => {
 
-  if (school === null) {
-    //TODO: API call to backend
-  }
+  const { id } = useParams<{ id: string }>();
+  const { schoolMap, isLoading } = useSchoolContext();
+
+  const school = id && schoolMap ? schoolMap.get(Number(id)) : null;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!school) return <Navigate to="/auth/404" replace />
 
   return (
     <>
-      {school === null ? (
-
-      )}
       <h1 className="font-semibold text-4xl pb-5">school.name</h1>
       <div className="lg:col-span-8 col-span-12 pt-8">
         <SchoolInfoCards school={school} />
