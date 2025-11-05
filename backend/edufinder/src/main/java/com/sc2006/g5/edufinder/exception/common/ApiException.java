818415a953e9f34c4cb1f424219e07eebb47be8b
yongcs106 <yong106106@gmail.com>
2025-11-05 18:02:04 +0800

    test(config): add unit test for GlobalExceptionHandler

--- backend/edufinder/src/main/java/com/sc2006/g5/edufinder/exception/common/ApiException.java
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/exception/common/ApiException.java
@@ -1,17 +1,16 @@
 package com.sc2006.g5.edufinder.exception.common;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class ApiException extends RuntimeException {
      
     private final HttpStatus httpStatus;
 
-    protected ApiException(String message, HttpStatus httpStatus){
+        public ApiException(String message, HttpStatus httpStatus){
         super(message);
         this.httpStatus = httpStatus;
     }
 
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
 }
