c838780b07bfbb6c3f3c52347b21c3b0205e40f4
Yu Wenhao <yuwenhao03@gmail.com>
2025-11-09 17:08:44 +0800

    Add SavedSchools page

--- edufinder_frontend/src/views/dashboards/SavedSchools.tsx
+++ edufinder_frontend/src/views/dashboards/SavedSchools.tsx
@@ -1,33 +1,55 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import CONSTANTS from 'src/constants.ts';
+import toast from 'react-hot-toast';
+import TopSchools from 'src/components/dashboard/TopSchools.tsx';
+import { useSchoolContext} from 'src/context/SchoolProvider.tsx';
 
 const SavedSchools = () => {
 
-  useEffect(() => {
-    fetch(CONSTANTS.backendEndpoint + '/users/saved-schools', {
-      method: 'GET',
-      credentials: 'include',
+  const { schoolMap, isLoading } = useSchoolContext();
+  const [savedSchoolIds, setSavedSchoolIds] = useState<number[]>([]);
+  const [savedSchoolLoading, setSavedSchoolLoading] = useState(true);
 
-    })
-      .then(res => {
-        console.log('Raw response:', res);
-        return res.json();
-      })
-      .then(data => {
-        console.log('Parsed data:', data);
-        // Do something with the data here
-      })
-      .catch(err => {
-        console.error('Fetch error:', err);
-      });
+  useEffect(() => {
+    const fetchSavedSchools = async () => {
+      try {
+        const res = await fetch(CONSTANTS.backendEndpoint + '/users/saved-schools', {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (!res.ok) throw new Error('Failed to fetch saved schools');
+
+        const data = await res.json();
+        console.log("Saved schools: ", data);
+        setSavedSchoolIds(data.savedSchoolIds);
+      } catch (err) {
+        console.error('Error fetching saved schools', err);
+        toast.error('Failed to load saved schools');
+      } finally {
+        setSavedSchoolLoading(false);
+      }
+    };
+
+    fetchSavedSchools();
   }, []);
+
+  const savedSchools = savedSchoolIds
+    .map(id => schoolMap[id])
+    .filter(Boolean);
 
   return (
     <>
       <h1 className="font-semibold text-4xl pb-5 text-primary">Saved Schools</h1>
       <div className="lg:col-span-8 col-span-12 pt-8">
 
-
+        {
+          savedSchoolLoading || isLoading  ? (
+            <p>Loading...</p>
+          ) : (
+            <TopSchools schools={savedSchools} />
+          )
+        }
       </div>
     </>
   );
