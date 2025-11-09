3d2cae0f586c48c89e8b31fa9e84de53dbec410d
yongcs106 <yong106106@gmail.com>
2025-11-09 19:20:32 +0800

    fix(security): fix config to allow preflight request

--- backend/edufinder/src/main/java/com/sc2006/g5/edufinder/config/SecurityConfig.java
+++ backend/edufinder/src/main/java/com/sc2006/g5/edufinder/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .requestMatchers("/error").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/schools/**").permitAll()
