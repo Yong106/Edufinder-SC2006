e96dda66e161e92b193ee9ed85d4c7ac96b207db
Yu Wenhao <yuwenhao03@gmail.com>
2025-11-15 23:07:36 +0800

    Refactor edufinder_frontend to frontend

--- frontend/edufinder/next.config.mjs
+++
@@ -1,9 +0,0 @@
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    images: { unoptimized: true }
-};
-
-export default withFlowbiteReact(nextConfig);
